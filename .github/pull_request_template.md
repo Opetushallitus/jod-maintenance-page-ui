3d8f35bcdeb0a78228197b1f3a18f1011add8f09
Sauli Anto <sauli.anto@gofore.com>
2025-12-18 08:38:20 +0200

    OPHJOD-2815: Add project base

---
+++ .github/pull_request_template.md
@@ -0,0 +1,18 @@
+<!-- Did builds complete successfully?
+Remembered to run linters?
+-->
+
+## Description
+
+<!-- Give some description about the PR.
+- What was done and why? Give some context to help the reviewer.
+- Where to focus especially?
+-->
+
+## Related JIRA ticket
+
+<!-- Remember to add link to the JIRA issue
+https://jira.eduuni.fi/browse/OPHJOD-XXX
+-->
+
+https://jira.eduuni.fi/browse/OPHJOD-
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/huoltokatko/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
+});
