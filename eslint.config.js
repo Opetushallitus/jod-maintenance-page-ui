4427b9850469ce25d416576aec38135d3652c0de
Sauli Anto <sauli.anto@gofore.com>
2026-03-19 15:32:16 +0200

    OPHJOD-3209: Refactor ESLint configuration to use defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -5,30 +5,38 @@ import jsxA11y from 'eslint-plugin-jsx-a11y';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist', 'scripts/**'] },
-  { files: ['**/*.{ts,tsx}'] },
-  { languageOptions: { ecmaVersion: 2020, globals: globals.browser } },
-  jsxA11y.flatConfigs.recommended,
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+export default defineConfig([
+  globalIgnores(['dist']),
   {
+    files: ['**/*.{ts,tsx}'],
     plugins: {
-      react: react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
       '@singlestore/react-hooks-disable-import': singlestoreReactHooksDisableImport,
     },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@singlestore/react-hooks-disable-import/react-hooks-disable-import': 'error',
-      'react/no-unstable-nested-components': 'warn',
-      'react/no-array-index-key': 'warn',
     },
   },
   eslintConfigPrettier,
-);
+]);
